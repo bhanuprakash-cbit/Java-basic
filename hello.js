8e6c1227be0d22faa9442f33f38854645770a370
Bhanu Prakash <bhanuprakash.mymail@gmail.com>
2022-12-06 10:07:01 +0000

    Create hello.js and index.js

---
+++ hello.js
@@ -0,0 +1,41 @@
+// var a='65499'
+// console.log(a)
+// a=100
+// let b
+// b=124
+// const c=21
+// if (a>b){
+//     console.log('a is greater than b')
+// }else{
+//     console.log('b is greater than a')
+// }
+
+const n=10
+//for loop
+//for(intialisation;condition;increment or decrement){
+//    stmtsss
+//}
+for(let i=0;i<n;i++){
+    console.log(i)
+}const a={
+    name: 'BHANU'
+    college{
+        name1: 'CBIT'
+        address: 'Gandipet'
+    }
+}
+const arr=['654321',BHANUPRAKASH,3216549]
+cosole.log()
+try{
+    console.log(a.college.name)
+}catch(err){
+    console.log('Error occured')
+}
+
+
+function square(n){
+    return n*n
+}
+console.log(square(321565n))
+
+
